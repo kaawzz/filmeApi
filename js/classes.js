efe07c4d86b64f4cd44b436b6e34ffa414082bb8
Raissa <raissaschunck2005@gmail.com>
2023-03-21 19:56:10 -0300

    parte 2

---
+++ js/classes.js
@@ -0,0 +1,29 @@
+class Ator{
+    constructor(id, nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+
+class Diretor{
+    constructor(id, nome){
+        this.nome=nome;
+        this.id=id;
+    }
+}
+
+class Filme{
+    constructor(id, titulo, ano, genero, duracao, cartaz, sinopse, direcao, elenco, classificacao, avaliacao){
+        this.id=id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.genero=genero;
+        this.duracao=duracao;
+        this.cartaz=cartaz;
+        this.sinopse=sinopse;
+        this.direcao=direcao;
+        this.elenco=elenco;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+    }
+}
